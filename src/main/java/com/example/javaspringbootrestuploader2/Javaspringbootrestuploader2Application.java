fcff0bf9ec34e989014c8700e43acc2ab9dd48ff
Aaron Shaver <aaronshaverpdx@gmail.com>
2021-01-25 14:42:20 -0800

    local machine commit

---
+++ src/main/java/com/example/javaspringbootrestuploader2/Javaspringbootrestuploader2Application.java
@@ -0,0 +1,13 @@
+package com.example.javaspringbootrestuploader2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Javaspringbootrestuploader2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Javaspringbootrestuploader2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javaspringbootrestuploader2/Javaspringbootrestuploader2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaspringbootrestuploader2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Javaspringbootrestuploader2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
