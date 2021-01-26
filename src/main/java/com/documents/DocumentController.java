bfd5190ddbbe849fb3fcfa5c50a71db33dd3a793
Aaron Shaver <aaronshaverpdx@gmail.com>
2021-01-25 17:20:07 -0800

    remove debugging; add hello endpoint

--- src/main/java/com/documents/DocumentController.java
+++ src/main/java/com/documents/DocumentController.java
@@ -2,11 +2,7 @@ package com.documents;
 
 import java.util.List;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class DocumentController {
@@ -37,4 +33,8 @@ public class DocumentController {
                 .orElseThrow(() -> new DocumentNotFoundException(id));
     }
 
+    @RequestMapping("/hello")
+    public String sayHello() {
+        return "Hello, world!";
+    }
 }
